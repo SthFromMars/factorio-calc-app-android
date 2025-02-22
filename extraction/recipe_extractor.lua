28bc67ca6c04dde0502186c5b8711f9757a45f43
SthFromMars <jokubas.ruskys@gmail.com>
2025-02-22 14:37:13 +0200

    get Factorio: Space Age recipes

---
+++ extraction/recipe_extractor.lua
@@ -0,0 +1,19 @@
+/c
+local recipes_table = {};
+for name, recipe in pairs(game.player.force.recipes) do
+    recipes_table[name] = {
+        name = recipe.name,
+        enabled = recipe.enabled,
+        category = recipe.category,
+        ingredients = recipe.ingredients,
+        products = recipe.products,
+        hidden = recipe.hidden,
+        energy = recipe.energy,
+        productivity_bonus = recipe.productivity_bonus
+    };
+end
+helpers.write_file(
+    "recipes.json",
+    helpers.table_to_json(recipes_table),
+    false
+);
